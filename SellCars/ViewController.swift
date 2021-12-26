ca8fb1043b9bd7215eaa66ce1fe69e1fb53ec5f2
ArtyomSolovev <artyom.solovev@icloud.com>
2021-12-26 17:06:19 +0300

    Initial Commit

---
+++ SellCars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SellCars
+//
+//  Created by Артем Соловьев on 26.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
